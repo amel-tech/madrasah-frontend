0d6addf915c6970025460a8c8428b6a163a10a3a
Samet <hey@samet.codes>
2025-11-01 19:30:34 +0300

    fix(dropdown-menu): replace CircleIcon with CheckIcon in radio item indicator

--- shared/ui/src/components/dropdown-menu.tsx
+++ shared/ui/src/components/dropdown-menu.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
-import { CheckIcon, CaretRightIcon, CircleIcon } from "@madrasah/icons"
+import { CheckIcon, CaretRightIcon } from "@madrasah/icons"
 
 import { cn } from "@madrasah/ui/lib/utils"
 
@@ -135,7 +135,7 @@ function DropdownMenuRadioItem({
     >
       <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
         <DropdownMenuPrimitive.ItemIndicator>
-          <CircleIcon className="size-2 fill-current" />
+          <CheckIcon className="size-4 fill-current" />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
