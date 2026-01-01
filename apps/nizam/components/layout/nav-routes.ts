a5ce1d73968d855b79eb10f255592c4416e03bd5
Samet <hey@samet.codes>
2026-01-01 17:55:38 +0300

    feat(nizam): implement course creation wizard

--- apps/nizam/components/layout/nav-routes.ts
+++ apps/nizam/components/layout/nav-routes.ts
@@ -1,4 +1,4 @@
-import { Icon, TableIcon } from '@madrasah/icons'
+import { Icon, TableIcon, House } from '@madrasah/icons'
 
 export type NavigationRouteType = {
   title: string
@@ -16,6 +16,11 @@ export const routes: {
       title: 'Decks',
       url: '/decks',
       icon: TableIcon,
+    },
+    {
+      title: 'Köşkler',
+      url: '/kosks',
+      icon: House,
     },
   ],
 }
