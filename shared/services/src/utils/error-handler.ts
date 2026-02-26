e1f290a877ee742aa4adcb6728a6e3d50fe47bdd
Samet <heysamet@pm.me>
2026-02-26 13:40:42 +0300

    refactor(error-handler): remove console logs that were left for debugging

--- shared/services/src/utils/error-handler.ts
+++ shared/services/src/utils/error-handler.ts
@@ -1,10 +1,7 @@
 export const getErrorMessage = (error: any): string => {
-  console.log(error);
   const itHasAtLeastOneError = error?.context?.errors && Array.isArray(error.context.errors) && error.context.errors.length > 0
-  console.log(itHasAtLeastOneError);
   if (itHasAtLeastOneError) {
     const firstError = error.context.errors[0]
-    console.log(firstError);
     if (firstError.constraints && typeof firstError.constraints === 'object') {
       const firstConstraintKey = Object.keys(firstError.constraints)[0]
       if (firstConstraintKey) {
